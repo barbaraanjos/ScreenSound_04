47fdc44a82f26d21b0bac06e86eeb0606689ea5e
barbara.anjos <barbara.anjos@segfy.com>
2024-06-07 16:13:45 -0300

    Add project files.

---
+++ ScreenSound_04/Filtros/LinqFilter.cs
@@ -0,0 +1,48 @@
+﻿using ScreenSound_04.Modelos;
+
+namespace ScreenSound_04.Filtros;
+
+internal class LinqFilter
+{
+	public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+	{
+		var todosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+
+		foreach(var genero in todosOsGenerosMusicais)
+		{
+            Console.WriteLine($"- {genero}");
+        }
+	}
+
+	public static void FiltrarArtistasPorGeneroMusical(List<Musica> musicas, string genero) 
+	{
+		var artistasPorGeneroMusical = musicas.Where(musica => musica.Genero!.Contains(genero)).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine($"Exibindo os artistas por gênero musical: {genero}");
+		foreach(var artista  in artistasPorGeneroMusical)
+		{
+            Console.WriteLine($"- {artista}");
+        }
+    }
+
+	public static void FiltrarMusicasDeUmArtista(List<Musica> musicas, string nomeDoArtista)
+	{
+		var musicaDoArtista = musicas.Where(musica => musica.Artista!.Equals(nomeDoArtista)).ToList();
+        Console.WriteLine($"Exibindo as músicas do artista: {nomeDoArtista}");
+
+		foreach (var musica in musicaDoArtista)
+		{
+			Console.WriteLine($"- {musica.Nome}");
+		}
+    }
+
+	public static void FiltrarMusicasPorAno(List<Musica> musicas, string ano)
+	{
+		var anoDaMusica = musicas.Where(musica => musica.Ano!.Equals(ano)).ToList();
+		Console.WriteLine($"Exibindo as músicas lançadas em {ano}");
+
+		foreach (var musica in anoDaMusica)
+		{
+			Console.WriteLine($"- Música: {musica.Nome}, Intérprete: {musica.Artista}");
+		}
+	}
+}
---
+++ ScreenSound_04/Program.cs
@@ -0,0 +1,24 @@
+﻿using ScreenSound_04.Filtros;
+using ScreenSound_04.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+
+	try
+	{
+		string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+		var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        Console.WriteLine(musicas.Count);
+		//LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+		//LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+		//LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
+		//LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+		LinqFilter.FiltrarMusicasPorAno(musicas, "2017");
+    }
+	catch (Exception ex)
+	{
+		Console.WriteLine($"Tivemos um problema: {ex.Message}");
+	}
+
+}
